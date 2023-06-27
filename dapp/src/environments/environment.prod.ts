9eaf57460ef8d469ec679b80b7c6bd565c5fd00e
layinka <xcelsis02@gmail.com>
2023-06-27 18:32:35 +0100

    added testnet deployed addresses to prod env

--- dapp/src/environments/environment.prod.ts
+++ dapp/src/environments/environment.prod.ts
@@ -6,15 +6,15 @@
 // };
 
 
-// theta_t
+// // fantom_t
 export const environment = {
-  production: true,
+  production: false,
   walletConnectProjectId: "4deab0af02ccba3c9813a2bb585e4a07",
-  eventManagerAddress: "0xE3cB58467250bd4178d737A87B87dc7AE00Dad62", // "0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5",
+  eventManagerAddress: "0x54B15d18c32032767d55AEB199ae488708cF6845", // "0x2B0d36FACD61B71CC05ab8F3D2355ec3631C0dd5",
   eventHelperAddress: "0xb2fd4d66a204676DC71d49819CE41480780Bf195",
   // apiUrl: 'https://localhost:3123/',
   apiUrl: 'https://test-tb.heirtrust.com/',
-  faucetAddress: "0x245835214BBBB7caD494eDE7903394c09F3b2f90", // "0x59b670e9fA9D0A427751Af201D676719a970857b",
-  ticketMarketAddress: '0x9ED133F814534B89c530909b9EfBAf226e6C9A4f',
-  seatAsigner: "0x8fcA49A066455dee7312E3900D8F67345631b82A"
+  faucetAddress: "0x7EaCAC40f0474F846A4A313Dc668177098B530bE", // "0x59b670e9fA9D0A427751Af201D676719a970857b",
+  ticketMarketAddress: '0x00aC989ddB7aEc2405a7f456De9E432c60A94283',
+  seatAsigner: "0xCbBAFCE8575ed84ec5219e72E48Eb6a7ad2234c5" //"0x8fcA49A066455dee7312E3900D8F67345631b82A"
 };
